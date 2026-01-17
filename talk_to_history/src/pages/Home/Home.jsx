dee46d7fc58e5ddd520878237e6c72cf5e97e9d1
Nurislom3000 <nurislomnavdar1@gmail.com@gmail.com>
2026-01-17 16:44:38 +0500

    s

---
+++ talk_to_history/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SamarkandWallpaper from '../../public/SamarkandWallpaper.jpg';
+
+const Home = () => {
+    return (
+        <div className="Home">
+
+        </div>
+    );
+};
+
+export default Home;
